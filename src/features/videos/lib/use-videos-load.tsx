6ffd0688da13f18eef38ee09ee7739fa5ddfec20
Konstaphy <Konstaphy@gmail.com>
2023-06-21 18:22:13 +0500

    видубрал лишнее

--- src/features/videos/lib/use-videos-load.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "react-query";
-import { videosTransport } from "../api/videos-transport";
-
-export const useVideosLoad = () => {
-  return useQuery("videos", () => {
-    return videosTransport.getAllVideos();
-  });
-};
